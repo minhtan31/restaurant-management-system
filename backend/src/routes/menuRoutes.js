da9013d7b393e0a67ec1e0aa25a81d7ff4d4411c
Uyennhii132 <nhi_2251220245@dau.edu.vn>
2026-05-04 11:36:56 +0700

    uppdate menu routes

--- backend/src/routes/menuRoutes.js
+++ backend/src/routes/menuRoutes.js
@@ -4,13 +4,14 @@ const {
   getMenuItems, getMenuItem, createMenuItem, updateMenuItem, deleteMenuItem,
 } = require('../controllers/menuController')
 const { protect, adminOnly } = require('../middleware/auth')
+const { uploadImage } = require('../middleware/upload')
 const { validate } = require('../middleware/validate')
 const { validateCreateMenu, validateUpdateMenu } = require('../validators/menuValidator')
 
 router.get('/', getMenuItems)
-router.post('/', protect, adminOnly, validate(validateCreateMenu), createMenuItem)
+router.post('/', protect, adminOnly, uploadImage, validate(validateCreateMenu), createMenuItem)
 router.get('/:id', getMenuItem)
-router.put('/:id', protect, adminOnly, validate(validateUpdateMenu), updateMenuItem)
+router.put('/:id', protect, adminOnly, uploadImage, validate(validateUpdateMenu), updateMenuItem)
 router.delete('/:id', protect, adminOnly, deleteMenuItem)
 
 module.exports = router
